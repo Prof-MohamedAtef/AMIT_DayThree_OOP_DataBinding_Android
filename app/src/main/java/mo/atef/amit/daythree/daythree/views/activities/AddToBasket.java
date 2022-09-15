f6ec2d0b3a96339aaebde581f96516ba7005e1ce
Mohamed Atef <proofesser@gmail.com>
2022-09-15 21:47:03 +0200

    AddToBasket.java and layout

---
+++ app/src/main/java/mo/atef/amit/daythree/daythree/views/activities/AddToBasket.java
@@ -0,0 +1,16 @@
+package mo.atef.amit.daythree.daythree.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mo.atef.amit.daythree.daythree.R;
+
+public class AddToBasket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_basket);
+    }
+}
